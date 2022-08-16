1f0005a5aee40b828f309750810045d4fbf3c09a
Archit Mathur (achie27) <architmathur2011@gmail.com>
2022-08-16 23:43:10 +0530

    feat(WIP): makes the main page look good

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,49 +4,145 @@ import Image from 'next/image'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/router'
 import { useTwitterContext } from '../providers/TwitterContext'
+import styled from 'styled-components'
+import { useEffect, useState } from 'react'
 
+const headers = [
+  "Had enough of sports updates from your adrenaline-spiked friend for today?",
+  "Don't want that content creator shoving brand-sponsored tweets in your timeline?",
+  "Too many political tweets from that domain expert you respect(ed)?",
+  "Not feeling particularly happy on seeing Happy New Year tweets?"
+]
+
+const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MainContainer = styled.div`
+  width: 60%;
+  margin: auto;
+`;
+
+const TweederDesc = styled.div`
+  position: relative;
+  float: left;
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-top: 15%;
+  padding: 10px;
+`
+
+const TweederHeadline = styled.div`
+  font-size: 50px;
+  font-weight: bolder;
+`
+
+const TweederSubHeadline = styled.div`
+  text-align: left;
+  font-size: 25px;
+  color: rgb(22 24 28);
+  margin: 20px 0;
+  `
+
+const TweederLogin = styled.div`
+  position: relative;
+  float: right;
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  margin-top: 15%;
+`
+
+const TweederLoginBox = styled.div`
+  width: 300px;
+  padding: 30px;
+  border:0;
+  border-radius: 20px;
+  border-color: white;
+  outline: 0;
+  background-color: rgb(22 24 28);
+  color: white;
+`
+
+const TweederLoginHeaderContainer = styled.div`
+`;
+
+const TweederLoginHeader = styled.div`
+  font-size: 50px;
+  font-weight: 200;
+`;
+
+const TweederLoginSubHeader = styled.div`
+
+`;
+
+const TweederLoginButtonContainer = styled.div`
+  margin-top: 30px;
+`;
+
+const TweederLoginButton = styled.div`
+  text-align: center;
+  padding: 20px;
+  text-decoration: none;
+  background-color: #1d9bf0;
+  border:0;
+  border-radius: 40px;
+  border-color: white;
+  outline: 0;
+  color: white;
+  font-weight: bold;
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 const Home: NextPage = () => {
   const { loginStatus } = useTwitterContext()
   const router = useRouter()
 
-  let loginSpace = <></>;
+  const [header, setHeader] = useState("");
+  useEffect(() => {
+    setHeader(headers[Date.now() % headers.length]);
+    setInterval(() => {
+      setHeader(headers[Date.now() % headers.length]);
+    }, 10 * 1000)
+  }, [])
 
-  switch (loginStatus) {
-    case "authenticated":
-      router.push("/dashboard");
-      return <></>;
-    case "loading":
-      loginSpace = <>Checking your session</>
-      break
-    case "unauthenticated":
-      loginSpace = <div className="maincontentright-login"> 
-      <button className="maincontentright-login-button" onClick={() => signIn("twitter", { redirect: false })}>Sign In</button>
-    </div>
-      break 
+  if (loginStatus === "authenticated") {
+    router.push("/dashboard");
+    return <></>;
   }
 
-  return (
-    <div>
-      <Head>
-        <title>Tweeder - tend to your Twitter timeline</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="maincontent">
-        <div className="maincontentleft">
-          Filter your tweet topics you don't want to see from certain authors 
-        </div>
-        <div className="maincontentright">
-          <div className="maincontentright-text">
-            Login to your Twitter account
-          </div>
-          {loginSpace}
-        </div>
-      </main>
-
-     </div>
-  )
+  return <>
+    <Main>
+      <MainContainer>
+        <TweederDesc>
+          <TweederHeadline>{header}</TweederHeadline>
+          <TweederSubHeadline>Use Tweeder to weed out tweets on certain domains made by certain authors from your timeline</TweederSubHeadline>
+        </TweederDesc>
+        <TweederLogin>
+          <TweederLoginBox>
+            <TweederLoginHeaderContainer>
+              <TweederLoginHeader>Tweeder</TweederLoginHeader>
+                <TweederLoginSubHeader>Tend to your Twitter timeline</TweederLoginSubHeader>
+            </TweederLoginHeaderContainer>
+            <TweederLoginButtonContainer>
+              {
+                loginStatus === "unauthenticated" ?
+                  <TweederLoginButton onClick={() => signIn("twitter", { redirect: false })}>Sign in with Twitter</TweederLoginButton>
+                :
+                <TweederLoginButton>Checking your session ...</TweederLoginButton>
+              }
+            </TweederLoginButtonContainer>
+          </TweederLoginBox>
+        </TweederLogin>
+      </MainContainer>
+    </Main>
+  </>
 }
 
 export default Home
